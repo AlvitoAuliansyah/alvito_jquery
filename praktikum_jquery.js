601cd8b28f35cd30bb722c2b89ebfe05913e166c
AlvitoAuliansyah <alauliansyah01@gmail.com>
2021-04-10 22:14:06 +0700

    first commit

---
+++ praktikum_jquery.js
@@ -0,0 +1,41 @@
+document.getElementById('nama').innerHTML = "Muhammad Alvito Masuk Surga, Aamiin";
+$('#nim').html("20190140120");
+$(function() {
+    console.log( "Hello" );
+});
+
+    $("#hide").on("click", function() {
+        $("#nama").hide();
+    });
+    
+    $("#show").on("click", function() {
+        $("#nama").show();
+    });
+    
+    $("#toggle").on("click", function() {
+        $("#nama").toggle();
+    });
+
+    $("#fadeIn").on("click", function() {
+        $("#tanggal").fadeIn();
+    });
+
+    $("#fadeOut").on("click", function() {
+        $("#tanggal").fadeOut();
+    });
+
+    $("#fadeToggle").on("click", function() {
+        $("#tanggal").fadeToggle();
+    });
+
+    $("#slideUp").on("click", function() {
+        $("#img").slideUp();
+    });
+
+    $("#slideDown").on("click", function() {
+        $("#img").slideDown();
+    });
+    
+    $("#slideToggle").on("click", function(){
+        $("#img").slideToggle();
+    });
